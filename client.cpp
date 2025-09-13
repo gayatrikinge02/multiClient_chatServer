7fa4dc4c04ad2e4612fb97210f992bfd95be35e9
gayatrikinge <gayatrikinge47@gmail.com>
2025-09-13 19:24:52 +0000

    created a single client server that sends and receives msg

--- client.cpp
+++ client.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-using namespace std;
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<cstring>
 
 int main() {
-    cout << "Hello from WSL!" << endl;
+   int lsocket=0;
+   struct sockaddr_in  l_serverAddress;
+   char lbuffer[1024]={0};
+
+   lsocket=socket(AF_INET,SOCK_STREAM,0);
+   if(lsocket<0){
+    std::cerr<<"couldnt create socket at client side";
+    return -1;
+}
+    l_serverAddress.sin_family=AF_INET;
+    l_serverAddress.sin_port=htons(8080);
+
+    if(inet_pton(AF_INET,"127.0.0.1",&l_serverAddress.sin_addr)<=0){
+        std::cerr<<"Invalid address"<<std::endl;
+        return -1;
+    }
+
+    if(connect(lsocket,(struct sockaddr*) &l_serverAddress,sizeof(l_serverAddress))<0){
+        std::cerr<<"Connection failed"<<std::endl;
+        return -1;
+    }
+
+    const char *hello = "Hello from client";
+    send(lsocket, hello, strlen(hello), 0);
+    std::cout << "Hello message sent" << std::endl;
+    read(lsocket, lbuffer, 1024);
+    std::cout<<"server msg:"<<lbuffer<<std::endl;
+
+    close(lsocket);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
-using namespace std;
+#include <unistd.h>
+#include <arpa/inet.h>
+#include<cstring>
+
 
 int main() {
-    cout << "Hello from WSL!" << endl;
-    return 0;
+    int lserverFd;
+    int lnewSocket;
+    struct sockaddr_in laddress;
+    memset(&laddress, 0, sizeof(laddress));  
+    int loption=1;
+    int laddLength=sizeof(laddress);
+    char lbuffer[1024]={0};
+
+    const char *hello="Greeting from server";
+    lserverFd=socket(AF_INET,SOCK_STREAM,0);
+    setsockopt(lserverFd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,&loption,sizeof(loption));
+    if(lserverFd<0){ perror("socket failed"); return 1; }
+    laddress.sin_family=AF_INET;
+    laddress.sin_addr.s_addr=INADDR_ANY;
+    laddress.sin_port=htons(8080);
+
+    if (bind(lserverFd, (struct sockaddr *)&laddress, sizeof(laddress)) < 0) {
+        perror("bind failed");
+        return 1;
+    }
+
+
+    listen(lserverFd,2);
+    std::cout<<"Server is listening on"<<std::endl;
+    lnewSocket=accept(lserverFd,(struct sockaddr*)&laddress,(socklen_t*)&laddLength);
+
+    read(lnewSocket,lbuffer,1024);
+    std::cout<<"buffer data"<<lbuffer<<std::endl;
+
+    send(lnewSocket,hello,strlen(hello),0);
+    std::cout<<"sent hello to client"<<std::endl;
+
+    close(lnewSocket);
+    close(lserverFd);
 }
